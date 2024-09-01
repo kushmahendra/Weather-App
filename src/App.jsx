ea5ebb82abdf908b7e4d449f8a8498a8317d78ac
Ahednarm <kushwahamahendra691@gmail.com>
2024-09-02 00:58:08 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Searchbar from './Components/Searchbar'
-import Weatherdetails from './Components/Weatherdetails'
-import Weatherforecast from './Components/Weatherforecast'
-import Locationweather from './Components/Locationweather'
 
 function App() {
   
 
   return (
     <>
+
      <Searchbar />
 
     </>
